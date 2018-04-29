ec54a66a99541260997ea8b7be41f36c366ac3af
Chris Fuess <cfuess@LIVE.COM>
2018-04-29 02:08:34 -0700

    Added Todo Pages

---
+++ MVC_ToDo/Controllers/TodoController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using MVC_ToDo.Models;
+
+namespace MVC_ToDo.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: Todo
+
+        public ActionResult Index()
+        {
+            return View(Todos.Items);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(GetItem(id));
+        }
+
+        [HttpPost]
+        public ActionResult Create(Todo changedItem)
+        {
+            Todos.Add(changedItem);
+            return View(changedItem);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(GetItem(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Todo changedItem)
+        {
+            Save(changedItem);
+            return View(GetItem(changedItem.Id));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item = GetItem(id);
+            Todos.Items.Remove(item);
+            return View("Index", Todos.Items);
+        }
+
+        private Todo GetItem(int id)
+        {
+            Todo matcheTodo = new Todo();
+            foreach (var item in Todos.Items)
+            {
+                if (item.Id == id)
+                {
+                    matcheTodo = item;
+                }
+            }
+            return matcheTodo;
+        }
+
+        private void Save(Todo changedItem)
+        {
+            var item = Todos.Items.First(l => l.Id == changedItem.Id);
+            item.Title = changedItem.Title;
+            item.Priority = changedItem.Priority;
+            item.Completed = changedItem.Completed;
+        }
+    }
+}
---
+++ MVC_ToDo/Models/Todo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Razor.Generator;
+
+namespace MVC_ToDo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public int Priority { get; set; }
+
+        public Todo()
+        {
+        }
+
+        public Todo(int id, string title, int priority = 1, bool completed = false)
+        {
+            Id = id;
+            Title = title;
+            Completed = completed;
+            Priority = priority;
+        }
+    }
+
+    public static class Todos
+    {
+        public static List<Todo> Items { get; set; } = new List<Todo>();
+        private static int _maxId;
+
+        public static void Load()
+        {
+            Items.Add(new Todo(1, "Clean House"));
+            Items.Add(new Todo(2, "Wash Car", 3));
+            Items.Add(new Todo(3, "Drink Water"));
+            Items.Add(new Todo(4, "Meditate", 2, true));
+            Items.Add(new Todo(5, "Walk"));
+            _maxId = 6;
+        }
+
+        public static void Add(Todo item)
+        {
+            Items.Add(new Todo(_maxId, item.Title, item.Priority, item.Completed));
+            _maxId++;
+        }
+
+    }
+}
+
